05ff2d56f18cb7482da0fa48217d97fbfeb135c7
Walukagga Patrick <pwalukagga@gmail.com>
2023-03-03 18:34:13 +0000

    AWS X-ray instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,12 +36,21 @@ provider.add_span_processor(simple_console_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-Ray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 app = Flask(__name__)
 
 # Honeycomb ------------------
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+# AWS X-RAY
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv("FRONTEND_URL")
 backend = os.getenv("BACKEND_URL")
