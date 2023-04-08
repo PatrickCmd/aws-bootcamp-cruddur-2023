55ac3d4c902b1b1af1cce2bc725d0f4cfadbb988
Patrick Walukagga <patrickcmdtelnet@gmail.com>
2023-04-08 21:21:36 +0000

    Implement refresh cognito jwt token

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,6 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import {getAccessToken} from '../utilities/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -18,11 +19,12 @@ export default function ActivityForm(props) {
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
-      console.log('onsubmit payload', message)
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          Authorization: `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
