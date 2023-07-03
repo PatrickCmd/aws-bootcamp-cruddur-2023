921e81dd2df943df6cfde6a1729f3028b871fdd3
Walukagga Patrick <pwalukagga@gmail.com>
2023-07-03 16:05:11 +0000

    CICD pipeline fix and create activity

--- backend-flask/services/users_short.py
+++ backend-flask/services/users_short.py
@@ -3,6 +3,7 @@ from utils.db import db
 
 class UsersShort:
     def run(handle):
+        """Return a user object"""
         sql = db.template("users", "short")
         results = db.query_object_json(sql, {"handle": handle})
         return results
