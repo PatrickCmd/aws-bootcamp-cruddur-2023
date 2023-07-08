350469625275f5c4e10059593de296518616d2cd
Walukagga Patrick <pwalukagga@gmail.com>
2023-07-08 13:57:10 +0000

    Create machine user cfn template

--- backend-flask/utils/ddb.py
+++ backend-flask/utils/ddb.py
@@ -19,7 +19,7 @@ class Ddb:
 
     def list_message_groups(client, my_user_uuid):
         current_year = datetime.now().year
-        table_name = "cruddur-messages"
+        table_name = os.getenv("DDB_MESSAGE_TABLE")
         query_params = {
             "TableName": table_name,
             "KeyConditionExpression": "pk = :pk AND begins_with(sk,:year)",
@@ -56,7 +56,7 @@ class Ddb:
 
     def list_messages(client, message_group_uuid):
         current_year = datetime.now().year
-        table_name = "cruddur-messages"
+        table_name = os.getenv("DDB_MESSAGE_TABLE")
         query_params = {
             "TableName": table_name,
             "KeyConditionExpression": "pk = :pk AND begins_with(sk,:year)",
@@ -109,7 +109,7 @@ class Ddb:
             "user_handle": {"S": my_user_handle},
         }
         # insert the record into the table
-        table_name = "cruddur-messages"
+        table_name = os.getenv("DDB_MESSAGE_TABLE")
         response = client.put_item(TableName=table_name, Item=record)
         # print the response
         print(response)
@@ -133,7 +133,7 @@ class Ddb:
         other_user_display_name,
         other_user_handle,
     ):
-        table_name = "cruddur-messages"
+        table_name = os.getenv("DDB_MESSAGE_TABLE")
 
         message_group_uuid = str(uuid.uuid4())
         message_uuid = str(uuid.uuid4())
