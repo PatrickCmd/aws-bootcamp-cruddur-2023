3cf413a6b0391079dde32d3e4d7acfa5b80a01a1
Patrick Walukagga <patrickcmdtelnet@gmail.com>
2023-07-09 18:15:03 +0000

    Migration update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -13,7 +13,7 @@ VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'patrickcmd' LIMIT 1),
     'This was imported as seed data!',
-    current_timestamp 0 interval '10 day'
+    current_timestamp + interval '10 day'
   ),
   (
     (SELECT uuid from public.users WHERE users.handle = 'patrickcmdtelnet' LIMIT 1),
